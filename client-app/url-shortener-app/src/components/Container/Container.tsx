741baa9622ca63e0a1b418f0ffa1a3497ca73b1f
Kiwi <chiekechidinma7@gmail.com>
2024-08-16 14:33:21 +0100

    capstone

--- client-app/url-shortener-app/src/components/Container/Container.tsx
+++ client-app/url-shortener-app/src/components/Container/Container.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import FormContainer from '../FormContainer/FormContainer';
+import { serverUrl } from '../../helpers/Constants';
+import axios from 'axios';
+import DataTable from '../DataTable/DataTable';
+import { UrlData } from '../../interface/UrlData';
 
 type IContainerProps = object
 
 const Container: React.FunctionComponent<IContainerProps> = () => {
+  const [data, setData] = React.useState<UrlData[]>([]);
+
+  const fetchTableData = async () => {
+    const response = await axios.get(`${serverUrl}/shortUrl`);
+    console.log("The response from server is : ", response);
+    setData(response.data);
+  };
+
+  React.useEffect(() => {
+    fetchTableData();
+  }, []);
   return (
     <>
     <FormContainer/>
+    <DataTable data={data} />
     </>
   );
 };
--- client-app/url-shortener-app/src/components/FormContainer/FormContainer.tsx
+++ client-app/url-shortener-app/src/components/FormContainer/FormContainer.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react';
+import axios from 'axios';
+import { serverUrl } from '../../helpers/Constants';
 
 type IFormContainerProps = object
 
 const FormContainer: React.FunctionComponent<IFormContainerProps> = () => {
-  return (
+  const [fullUrl, setFullUrl] = React.useState<string>("");
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+     e.preventDefault();
+     try {
+        await axios.post(`${serverUrl}/shorturl`, {
+            fullUrl: fullUrl,
+        });
+        setFullUrl("");
+     } catch (error) {
+        console.log(error);
+     };
+  };
+    return (
     <div className='container mx-auto p-2'>
         <div className='bg-slate-400 my-8 rounded-xl bg-cover bg-center'>
             <div className='w-full h-full rounded-xl p-20 backdrop-brightness-50'>
@@ -14,14 +28,19 @@ const FormContainer: React.FunctionComponent<IFormContainerProps> = () => {
             <p className='text-white text-center pb-4 text-sm font-extralight'>
                Free tool to shorten your URL.  Use URLShortener to create a simpler link.
             </p>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='flex'>
                     <div className='relative w-full'>
                         <div className='absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none text-slate-800'>
                             urlshortener.link /
                         </div>
-                        <input type='text' placeholder='Add your link' required className='block w-full p-4 ps-36 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500' />
-                        <button type='submit' className=''></button>
+                        <input type='text' placeholder='Add your link' required className='block w-full p-4 ps-36 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500' 
+                        value={fullUrl}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFullUrl(e.target.value)}
+                        />
+                        <button type='submit' className='absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-lg border border-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300'>
+                            Shorten URL
+                        </button>
                     </div>
                 </div>
             </form>
