45e94c84c00ee848468f91ea3f9d2415df791272
Kiwi <chiekechidinma7@gmail.com>
2024-08-19 20:06:49 +0100

    added api

--- client-app/url-shortener-app/src/helpers/Constants.ts
+++ client-app/url-shortener-app/src/helpers/Constants.ts
@@ -1,2 +1,2 @@
 export const serverUrl = 
-import.meta.env.VITE_SERVER_URL || "http://https://capstoneproject-tz05.onrender.com/api";
+import.meta.env.VITE_SERVER_URL || "https://capstoneproject-tz05.onrender.com/api";
